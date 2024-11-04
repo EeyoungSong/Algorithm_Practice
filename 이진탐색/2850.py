245d6559c61647e4124cdd89cffedb217734fcc9
eeyoungsong@gmail.com <eeyoungsong@gmail.com>
2024-11-05 01:02:23 +0900

    2850

---
+++ 이진탐색/2850.py
@@ -0,0 +1,7 @@
+# 나무 M 미터 
+# 절단기 높이 H -> 연속해서 자름 H보다 크면 잘림
+# 필요한 만큼만 가져갈 것
+# 적어도 M미터를 가져가기 위해 절단기에 설정할 수 있는 높이의 최대값
+import sys
+n, m = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
